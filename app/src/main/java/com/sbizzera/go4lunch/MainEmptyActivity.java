825bd4c3dbb3378f27754422958aa3c802e50148
bosbizz <bosbizz@gmail.com>
2019-11-02 15:13:37 +0100

    Working on LoginActivity. Google SignIn functionnal

--- app/src/main/java/com/sbizzera/go4lunch/MainEmptyActivity.java
+++ app/src/main/java/com/sbizzera/go4lunch/MainEmptyActivity.java
@@ -2,14 +2,28 @@ package com.sbizzera.go4lunch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainEmptyActivity extends AppCompatActivity {
+public class MainEmptyActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button goToLogInBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_empty);
+        goToLogInBtn = findViewById(R.id.go_to_log_in_btn);
+        goToLogInBtn.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this,LoginActivity.class);
+        startActivity(intent);
     }
 }
 
